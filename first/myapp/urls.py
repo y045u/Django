854c3c2c9db715a3da68f54093e4d961a3674c1c
y045u <fukasawa0405.tech@gmail.com>
2023-01-15 23:44:10 +0900

    base.htmlを利用して複数ページ作成

--- first/myapp/urls.py
+++ first/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'myapp'
 
 urlpatterns = [
-    path('', views.index, name='index'), # 127.0.0.1:8000/myapp
+    path('', views.index, name='index'),  # 127.0.0.1:8000/myapp
+    path('about/', views.about, name='about'),  # 127.0.0.1:8000/myapp/about
+    path('info/', views.info, name='info'),  # 127.0.0.1:8000/myapp/info
 ]
--- first/myapp/views.py
+++ first/myapp/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world")
+    """/ トップページ"""
+    context = {
+        'name': 'nayuta',
+    }
+    return render(request, 'myapp/index.html', context)
+
+
+def about(request):
+    """/about アバウトページ"""
+    return render(request, 'myapp/about.html')
+
+
+def info(request):
+    """/info インフォページ"""
+    return render(request, 'myapp/info.html')
